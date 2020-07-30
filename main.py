eb8f80ae6f03991d37a105a58aaed0b58aa12480
ant <polishka@cs.ucr.edu>
2020-07-29 20:55:54 -0700

    add pipeline from coronawhy/preprocessing_v19

---
+++ main.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+import random, argparse, os, json
+from preprocessutils import preprocess_metadata, chunking, str2bool
+from pipeline import pipeline
+from psutil import virtual_memory
+import pandas as pd
+
+
+
+def main():
+    
+    parser = argparse.ArgumentParser(description='Multi-CPU Preprocessing Pipeline for CORD19-v19.')
+    #path
+    parser.add_argument('--CORD19_path', type=str,default='./data',
+                    help='Path to CORD-19 path as provided on Kaggle platform.')
+    parser.add_argument('--output_path', type=str,default='output',
+                    help='Path to output directory')
+    #optional delta json file
+    parser.add_argument('--delta', type=str,
+                    help='Delta json file with file names (CORD19 SHA) already preprocessed.')
+    #max. number of files 
+    parser.add_argument('--max_n_files', type=int, default=None, const=10, nargs='?',
+                    help='Optional maximal number of files you want to preprocess (for development purposes)')
+    #supposed/desired amount of free RAM per worker 
+    parser.add_argument('--RAM_per_worker', type=int, default=12, const=12, nargs='?',
+                    help='Amount of RAM in GiBs that each worker should obtain. It restricts the number of engaged workers. Default value 12 GiB')
+    #en_core_sci_lg
+    parser.add_argument('--en_core_sci_lg', type=str2bool, default=True, nargs='?', const=True,
+                        help="A full spaCy pipeline for biomedical data with a larger vocabulary and 600k word vectors.")
+    #en_ner_craft_md
+    parser.add_argument('--en_ner_craft_md', type=str2bool, default=True, nargs='?', const=True,
+                        help="A spaCy NER model trained on the CRAFT corpus.")
+    #en_ner_jnlpba_md
+    parser.add_argument('--en_ner_jnlpba_md', type=str2bool, default=True, nargs='?', const=True,
+                        help="A spaCy NER model trained on the JNLPBA corpus.")
+    #en_ner_bc5cdr_md
+    parser.add_argument('--en_ner_bc5cdr_md', type=str2bool, default=True, nargs='?', const=True,
+                        help="A spaCy NER model trained on the BC5CDR corpus.")
+    #en_ner_bionlp13cg_md
+    parser.add_argument('--en_ner_bionlp13cg_md', type=str2bool, default=True, nargs='?', const=True,
+                        help="A spaCy NER model trained on the BIONLP13CG corpus.")
+    
+    
+    args = parser.parse_args()
+    
+    assert args.CORD19_path != None, "you should specify a path to CORD19 collection"
+    
+    folder_output = args.output_path
+
+    assert folder_output != None, "you should specify the path for output"
+
+    if args.delta == None:
+        print("No delta file specified. The pipeline will process the whole collection.")
+        delta_sha_list = False
+    else:
+        with open(args.delta) as f:
+            delta_file = json.load(f)
+            delta_sha_list = delta_file["delta list"]
+            delta_sha_list = ''.join(delta_sha_list)
+    
+    # # Preprocess the metadata to get folder and subfolder structre and the names of files
+    # files, paths_to_files = preprocess_metadata(args.CORD19_path)
+
+############## Insted doing v15+ paths to files directly
+    directory = args.CORD19_path
+    df = pd.read_csv(os.path.join(directory,"metadata.csv"), low_memory=False) 
+    paths_to_files = []
+    
+    #drop rows that don't contain full text jsons
+    df.dropna(subset=['pmc_json_files','pdf_json_files'], how='all', inplace=True)
+
+    df_pmc_only = df['pmc_json_files'].dropna()
+    for row in df_pmc_only:
+        paths_to_files.append( os.path.join(directory,row.split(';')[0]) )
+
+    df_pdf_only = df[df['pmc_json_files'].isna()]['pdf_json_files'].dropna()
+    for row in df_pdf_only:
+        paths_to_files.append( os.path.join(directory,row.split(';')[0]) )
+
+
+    #files, paths_to_files = files[:100], paths_to_files[:100]
+    if delta_sha_list:
+        old_doc_nubmer = len(files)
+        files_and_paths = [(file, file_path) for (file, file_path) in zip(files, paths_to_files) if file not in delta_sha_list]
+        print(len(files_and_paths))
+        files, paths_to_files = zip(*files_and_paths)
+        new_doc_number = len(files)
+        print("""
+           A Delta file for CORD19 database has been applied. 
+           Instead of {} files, only {} of them will be annotated. 
+        """.format(old_doc_nubmer, new_doc_number))
+        
+    
+    
+    #quit()
+    #paths_to_files = paths_to_files[:100]
+    #print(paths_to_files)
+    # file_size_list = [os.path.getsize(x) for x in paths_to_files]
+    # pathsAndFileSizes = list(zip(paths_to_files,file_size_list))
+    # paths_to_files_sorted, file_size_list_sorted = zip(*sorted(pathsAndFileSizes, key = lambda x: x[1]))
+    # paths_to_files_sorted = list(paths_to_files_sorted)
+    # random.shuffle(paths_to_files_sorted)
+    
+    # if args.max_n_files != None:
+    #     paths_to_files_sorted = paths_to_files_sorted[:args.max_n_files]
+    
+    print("""
+       We are going to process {} files from CORD19 database. 
+    """.format(len(paths_to_files)))
+       
+    
+    
+
+    
+    #gather SciSpacy model preferences and pack them into chunks for each process
+    model_dict = {"description":"""
+    A dictionary of SciSpacy model preferences specified by the user. 
+    By default all models will be loaded"""}
+    model_dict["en_core_sci_lg"] = args.en_core_sci_lg 
+    model_dict["en_ner_craft_md"] = args.en_ner_craft_md 
+    model_dict["en_ner_jnlpba_md"] = args.en_ner_jnlpba_md 
+    model_dict["en_ner_bc5cdr_md"] = args.en_ner_bc5cdr_md
+    model_dict["en_ner_bionlp13cg_md"] = args.en_ner_bionlp13cg_md
+    
+    models_selected = ["--"+k for k,v in model_dict.items() if type(v) == bool and v == True]
+
+    #send file path chunks to a pool of workers
+    results = pipeline(paths_to_files, model_dict, folder_output)
+    
+           
+if __name__ == "__main__":
+    main()
---
+++ pipeline.py
@@ -0,0 +1,232 @@
+def pipeline(paths_to_files, model_prefs_dict, folder_name):
+    
+    #this function is supposed to run within multiprocessing
+    #so the imports go this way...
+    import os, json, uuid, re, warnings
+    from tqdm import tqdm
+    import numpy as np
+    from langdetect import detect
+    from preprocessutils import init_nlp, init_ner, translate, extract_tables_from_json, further_clean_section
+    from collections import defaultdict
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+    nlp, linker = init_nlp() 
+    nlps = init_ner(model_prefs_dict)
+    
+    
+    
+    
+    
+    
+    languages = []
+    start_chars = []
+    end_chars = []
+    entities = []
+    texts = []
+    lemmas = []
+    vectors = []
+    sections = []
+    section_ids = []
+    _ids = []
+    columns = []
+    
+    translated = []
+    umls_ids = []
+
+    scispacy_ent_types = ['GGP', 'SO', 'TAXON', 'CHEBI', 'GO', 'CL', 'DNA', 'CELL_TYPE', 'CELL_LINE', 'RNA', 'PROTEIN', 
+                          'DISEASE', 'CHEMICAL', 'CANCER', 'ORGAN', 'TISSUE', 'ORGANISM', 'CELL', 'AMINO_ACID',
+                          'GENE_OR_GENE_PRODUCT', 'SIMPLE_CHEMICAL', 'ANATOMICAL_SYSTEM', 'IMMATERIAL_ANATOMICAL_ENTITY',
+                          'MULTI-TISSUE_STRUCTURE', 'DEVELOPING_ANATOMICAL_STRUCTURE', 'ORGANISM_SUBDIVISION',
+                          'CELLULAR_COMPONENT', 'PATHOLOGICAL_FORMATION']
+
+    pbar = tqdm(total=len(paths_to_files))
+    pbar.set_description("Preprocessing json files")
+    
+    
+    #iterate over a list of file paths
+    for path_to_file in paths_to_files:
+        
+        #create dict to store all annotations, lemmas, NERs etc...
+        preprocessed_file = dict()
+        #read in the json file
+        with open(path_to_file, "rb") as f:
+            json_file = json.load(f)
+            f.close()
+    
+        #add paper id to file dict
+        paper_id = json_file["paper_id"]
+        preprocessed_file["paper_id"] = paper_id
+        
+        #try to get abstract if extisting
+        try:
+            paper_abstract = json_file["abstract"]#[abstract_paragraph for abstract_paragraph in json_file["abstract"]]
+        except:
+            paper_abstract = False
+        preprocessed_file["abstract"] = paper_abstract
+        
+        #get the body text being a list of sections
+        body_text = json_file["body_text"]
+        #we will gather section names and ids in a list
+        preprocessed_file["text_body"] = []
+        #add abstract paragraphs to text body and keep the track of them
+        abstract_sections_nb = 0
+        if paper_abstract:
+            body_text = paper_abstract + body_text
+            abstract_sections_nb = len(paper_abstract)
+        
+        #quick check if body text has at all some text
+        all_text = ' '.join([x['text'] for x in body_text])
+        if len(all_text) > 5:
+            pass
+        else:
+            continue
+            pass#add some actions if the file doesn't have any text
+        
+        
+        
+        
+        #find out the language of the paper
+        #what to do if language other than EN? Translate or omit...?
+        try:
+            found_lang = detect(all_text[:1000])
+            preprocessed_file['language'] = found_lang
+        except:
+            warnings.warn('Language of this text body cannot be recognised: {}\nWe are skipping this text body'.format(all_text[:1000]))
+            continue
+        if found_lang != 'en':
+            #if language other than EN then use Google translator to translate section by section
+            original_text = '\n'.join(['\n'.join([section_body['section'], section_body['text']]) for section_body in body_text])
+            preprocessed_file["original_text"] = original_text
+            body_text = [{'text':translate(section_body['text']),'section':translate(section_body['section'])} for section_body in body_text]
+        else:
+            preprocessed_file["original_text"] = []
+        
+        
+        #extract tables if applicable
+        tables = extract_tables_from_json(json_file)
+        preprocessed_file["tables"] = tables
+        
+        
+        section_ids_list = []
+        #iterate over a list of sections
+        for section_nb, section_body in enumerate(body_text):
+            #print("---new section---")
+            #extract the text and the name of a section
+            section_text = section_body['text']
+            section_name = section_body['section']
+            section_name = further_clean_section(section_name)
+            section_id = str(uuid.uuid1())
+            #section_dict["section_id"] = section_id
+            
+            section_ids_list.append(section_id)
+            
+            
+            if section_nb < abstract_sections_nb:
+                preprocessed_file["abstract"].append(section_id)
+            else:
+                preprocessed_file["text_body"].append({"section_id":section_id,"section_name":section_name})
+                
+            if model_prefs_dict["en_core_sci_lg"]:
+                #let spacy digest the text content
+                doc = nlp(section_text)
+                #let unabbreviate the abbreviation
+                if len(doc._.abbreviations) > 0:
+                    doc._.abbreviations.sort()
+                    join_list = []
+                    start = 0
+                    for abbrev in doc._.abbreviations:
+                        join_list.append(str(doc.text[start:abbrev.start_char]))
+                        if len(abbrev._.long_form) > 5: #Increase length so "a" and "an" don't get un-abbreviated
+                            join_list.append(str(abbrev._.long_form))
+                        else:
+                            join_list.append(str(doc.text[abbrev.start_char:abbrev.end_char]))
+                        start = abbrev.end_char
+                    # Reassign fixed body text to article in df.
+                    section_text = "".join(join_list)
+                    # We have new text. Re-nlp the doc for futher processing!
+                    doc = nlp(section_text)
+                
+                section_sent_ids_list = []
+                for single_sentence in doc.sents:
+                    sentence_dict = {}
+                    sentence_id = str(uuid.uuid1())
+                    section_sent_ids_list.append(sentence_id)
+                    sentence_dict["sentence_id"] = sentence_id
+                    tokens = str(single_sentence.text)
+                    sentence_dict["tokens"] = tokens
+                    lemmas = [token.lemma_.lower() for token in single_sentence if not token.is_stop and re.search('[a-zA-Z]', str(token))]
+                    sentence_dict["lemmas"] = lemmas
+                    sent_ents = []
+                    for ent in single_sentence.ents: 
+                        if len(ent._.umls_ents) > 0:
+                            poss = linker.umls.cui_to_entity[ent._.umls_ents[0][0]].canonical_name
+                            sent_ents.append(poss)
+                    sentence_dict["umls"] = sent_ents
+                    umls_ids = [entity._.umls_ents[0][0] for entity in single_sentence.ents if len(entity._.umls_ents) > 0]
+                    sentence_dict["umls_ids"] = umls_ids
+                    sentence_vectors = [token.vector for token in single_sentence if not token.is_stop]
+                    if len(sentence_vectors)>0:
+                        sentence_vector = np.stack(sentence_vectors, axis=0).sum(0).tolist()
+                    else:
+                        sentence_vector = []
+                    sentence_dict["sent2vec"] = sentence_vector
+                    
+                    #preprocess each sentences also with additional SciSpacy models
+                    for special_nlp in nlps:
+                        single_sentence_special = special_nlp(tokens)
+                        keys = list(set([ent.label_ for ent in single_sentence_special.ents]))
+                        for key in keys:
+                            if key not in scispacy_ent_types:
+                                sentence_dict[key] = []
+                            values = [ent.text for ent in single_sentence_special.ents if ent.label_ == key]
+                            sentence_dict[key] = values
+                    #add sentence dictionary to the file dict
+                    preprocessed_file[sentence_id] = sentence_dict
+                #add list of sentence ids to the file dict
+                preprocessed_file[section_id] = section_sent_ids_list
+            #without en_core_sci_lg
+            else:
+                section_sent_ids_list = []
+                sentences = []
+                sentences_full_text = []
+                #run each of the smaller SciSpacy models selected by the user
+                for special_nlp in nlps:
+                    doc = special_nlp(section_text)
+                    model_sentences = []
+                    for single_sentence in doc.sents:
+                        sentence_dict = {}
+                        sentences_full_text.append(str(single_sentence.text))
+                        keys = list(set([ent.label_ for ent in single_sentence.ents]))
+                        for key in keys:
+                            if key not in scispacy_ent_types:
+                                sentence_dict[key] = []
+                            values = [ent.text for ent in single_sentence.ents if ent.label_ == key]
+                            sentence_dict[key] = values
+                        model_sentences.append(sentence_dict)
+                    sentences.append(model_sentences)
+                #We have k senteces from n models.
+                #Each model produced the same number of sentences. Let's zip them to have just k
+                #and add the original token strings.  
+                sents = list(zip(*sentences))
+                for sent_dicts, sent_full_text in zip(sents,sentences_full_text[:len(sents)]):
+                    sentence_dict = {}
+                    sentence_id = str(uuid.uuid1())
+                    section_sent_ids_list.append(sentence_id)
+                    sentence_dict["sentence_id"] = sentence_id
+                    sentence_dict["tokens"] = sent_full_text
+                    for element_dict in sent_dicts:
+                        if element_dict:
+                            for k, v in element_dict.items():
+                                sentence_dict[k] = v
+                    preprocessed_file[sentence_id] = sentence_dict
+               
+                preprocessed_file[section_id] = section_sent_ids_list
+                        
+        #save the preprocessed file on the disk
+        with open(os.path.join(folder_name,paper_id+".json"), "w", encoding="utf-8") as f:
+            json.dump(preprocessed_file, f, ensure_ascii=False)
+            f.close()
+        pbar.update()
+        
+    return True
+               
